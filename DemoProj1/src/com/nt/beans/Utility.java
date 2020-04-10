970d26d008a0630ffe0c74bb461f715dabc28045
TL <TL@gmail.com>
2020-04-10 23:07:44 +0530

    kfkfbkfkf

--- DemoProj1/src/com/nt/beans/Utility.java
+++ DemoProj1/src/com/nt/beans/Utility.java
@@ -13,4 +13,14 @@ public class Utility {
 		System.out.println("utility m1 () method:1234");
 		System.out.println("abc123");
 	}
+
+	public void m2()
+	{
+		System.out.println("m2");
+	}
+	public void m3()
+	{
+		System.out.println("m3");
+	}
+
 }
